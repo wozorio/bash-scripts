bed6d6e483b86467823720a60bd0f415c5fe96b9
Wellington Ozorio <45944705+wozorio@users.noreply.github.com>
2020-08-13 18:46:00 -0300

    Create azure-shutdown-all-vms.sh

---
+++ azure-shutdown-all-vms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######################################################################
+# Script Name    : azure-shutdown-all-vms.sh
+# Description    : Used to shutdown and deallocate all VMs in a subscription
+# Args           : n/a
+# Author         : Wellington Ozorio <well.ozorio@gmail.com>
+######################################################################
+
+# Stop execution on any error
+set -e
+
+# Stop all VMs
+az vm stop --ids $(
+  az vm list --query "[].id" --output tsv
+)
+
+# Deallocate all VMs
+az vm deallocate --ids $(
+  az vm list --query "[].id" --output tsv
+)
