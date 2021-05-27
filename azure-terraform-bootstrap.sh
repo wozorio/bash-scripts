657c38e53acfb2153d9b6b2df0473d73543fa839
Wellington Ozorio <Wellington.Ozorio@br.bosch.com>
2021-05-27 14:59:00 -0300

    bug fix

--- azure-terraform-bootstrap.sh
+++ azure-terraform-bootstrap.sh
@@ -17,7 +17,7 @@ function usage() {
 }
 
 # Check if the right number of arguments were passed
-if [[ $# -ne 3 ]]; then
+if [[ "$#" -ne 3 ]]; then
     usage
 fi
 
@@ -27,51 +27,56 @@ STORAGE_ACCOUNT_NAME=$3
 STORAGE_CONTAINER_NAME="tfstate"
 
 function create_resource_group() {
-    RESOURCE_GROUP_EXISTS=$(az group exists --name ${RESOURCE_GROUP_NAME})
+    RESOURCE_GROUP_EXISTS=$(az group exists --name "${RESOURCE_GROUP_NAME}")
 
-    if [[ ${RESOURCE_GROUP_EXISTS} == "true" ]]; then
+    if [[ "${RESOURCE_GROUP_EXISTS}" == "true" ]]; then
         echo "INFO: ${RESOURCE_GROUP_NAME} resource group already exists!"
     else
         echo "INFO: Creating resource group: ${RESOURCE_GROUP_NAME}"
-        az group create --location ${LOCATION} --name ${RESOURCE_GROUP_NAME}
+        az group create --location "${LOCATION}" --name "${RESOURCE_GROUP_NAME}"
     fi
 }
 
 function create_storage_account() {
-    STORAGE_ACCOUNT_EXISTS=$(az storage account check-name --name ${STORAGE_ACCOUNT_NAME} --query "nameAvailable")
+    STORAGE_ACCOUNT_EXISTS=$(az storage account check-name --name "${STORAGE_ACCOUNT_NAME}" --query "nameAvailable")
 
-    if [[ ${STORAGE_ACCOUNT_EXISTS} == "false" ]]; then
+    if [[ "${STORAGE_ACCOUNT_EXISTS}" == "false" ]]; then
         echo "INFO: ${STORAGE_ACCOUNT_NAME} storage account already exists!"
     else
         echo "INFO: Creating storage account: ${STORAGE_ACCOUNT_NAME}"
         az storage account create \
-            --name ${STORAGE_ACCOUNT_NAME} \
-            --resource-group ${RESOURCE_GROUP_NAME} \
-            --kind BlobStorage \
-            --location ${LOCATION} \
-            --sku Standard_LRS \
-            --https-only $true \
-            --allow-blob-public-access $false \
-            --min-tls-version TLS1_2
+        --name "${STORAGE_ACCOUNT_NAME}" \
+        --resource-group "${RESOURCE_GROUP_NAME}" \
+        --access-tier "Cool" \
+        --kind BlobStorage \
+        --location "${LOCATION}" \
+        --sku Standard_LRS \
+        --https-only $true \
+        --allow-blob-public-access $false \
+        --min-tls-version TLS1_2
     fi
 }
 
 function create_storage_container() {
-    STORAGE_ACCOUNT_KEY=$(az storage account keys list \
-        --account-name ${STORAGE_ACCOUNT_NAME} \
-        --query "[0].value")
+    STORAGE_ACCOUNT_KEY=$(
+        az storage account keys list \
+        --account-name "${STORAGE_ACCOUNT_NAME}" \
+        --query "[0].value"
+    )
 
-    STORAGE_CONTAINER_EXISTS=$(az storage container exists \
-        --name ${STORAGE_CONTAINER_NAME} \
-        --account-name ${STORAGE_ACCOUNT_NAME} \
-        --account-key ${STORAGE_ACCOUNT_KEY} \
-        --query "exists")
+    STORAGE_CONTAINER_EXISTS=$(
+        az storage container exists \
+        --name "${STORAGE_CONTAINER_NAME}" \
+        --account-name "${STORAGE_ACCOUNT_NAME}" \
+        --account-key "${STORAGE_ACCOUNT_KEY}" \
+        --query "exists"
+    )
 
-    if [[ ${STORAGE_CONTAINER_EXISTS} == "true" ]]; then
+    if [[ "${STORAGE_CONTAINER_EXISTS}" == "true" ]]; then
         echo "INFO: ${STORAGE_CONTAINER_NAME} storage container already exists!"
     else
         echo "INFO: Creating storage container: ${STORAGE_CONTAINER_NAME}"
-        az storage container create --name ${STORAGE_CONTAINER_NAME} --account-name ${STORAGE_ACCOUNT_NAME} --account-key ${STORAGE_ACCOUNT_KEY}
+        az storage container create --name "${STORAGE_CONTAINER_NAME}" --account-name "${STORAGE_ACCOUNT_NAME}" --account-key "${STORAGE_ACCOUNT_KEY}"
     fi
 }
 
